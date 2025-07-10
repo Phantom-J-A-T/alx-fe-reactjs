7f3596b1967572fd1a2bd306ffcab45c07105768
Phantom <ramonadedotun550@gmail.com>
2025-07-10 10:34:49 +0100

    Changing Welcome JSX file location

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomMessage'
+import WelcomeMessage from './components/WelcomMessage'
 
 function App() {
   const [count, setCount] = useState(0)
