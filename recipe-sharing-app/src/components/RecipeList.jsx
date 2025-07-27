c61f8c9255b4d87a4a79457ed54fbcb57c324657
Phantom <ramonadedotun550@gmail.com>
2025-07-27 02:51:16 +0100

    Adding a link component to the List

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,26 +1,30 @@
+import { Link } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
-    const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   if (filteredRecipes.length === 0) {
     return <p>No matching recipes found.</p>;
   }
 
-    return (
-      <div>
+  return (
+    <div>
       <h2>Recipes</h2>
       <ul>
         {filteredRecipes.map((recipe) => (
           <li key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>
+                {recipe.title}
+              </Link>
+            </h3>
             <p>{recipe.description}</p>
           </li>
         ))}
       </ul>
     </div>
-    );
-  };
+  );
+};
 
 export default RecipeList;
