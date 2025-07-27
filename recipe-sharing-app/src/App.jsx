4401573788ac1a76483df603ac2bf7f2f4959ed7
Phantom <ramonadedotun550@gmail.com>
2025-07-27 01:53:13 +0100

    Adding Zustand and creating the Recipe List app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './component/addrecipeform'
+import RecipeList from './component/recipelist'
+
+function App() {
+
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
