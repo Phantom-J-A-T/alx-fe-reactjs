8080270a751d33d783e35d2be2323c17c5ef0fe7
Phantom <ramonadedotun550@gmail.com>
2025-08-03 22:59:00 +0100

    Adding a Navbar and Footer to the Component folder

---
+++ github-user-search/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: '1rem', background: '#f0f0f0' }}>
+      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+      <Link to="/about" style={{ marginRight: '1rem' }}>About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
