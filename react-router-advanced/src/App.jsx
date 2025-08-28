8c0b79f919f6ed0c58e66a176930e29cc3655e87
Phantom <ramonadedotun550@gmail.com>
2025-08-29 00:52:14 +0100

    Creating the Dynamic routes components for the Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/profile" element={<Profile />}>
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
+          <Route path="/posts/:postId" element={<BlogPost />} />
         </Route>
       </Routes>
     </Router>
